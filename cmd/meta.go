b24f5e4f339b13e1609aad1ed6a4f7470db3d59f
Thorben Günther <admin@xenrox.net>
2020-02-27 15:57:38 +0100

    cobra: add meta

---
+++ cmd/meta.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var metaCmd = &cobra.Command{
+	Use:   "meta",
+	Short: "Use the srht meta API",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(metaCmd)
+}
