49f316327270b763a849f2eaf0011385bf887d38
Thorben Günther <admin@xenrox.net>
2020-03-02 21:12:14 +0100

    cmd/meta: add profile

--- cmd/meta.go
+++ cmd/meta.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"git.xenrox.net/~xenrox/srhtctl/api"
+	"git.xenrox.net/~xenrox/srhtctl/helpers/errorhelper"
 	"github.com/spf13/cobra"
 )
 
@@ -11,6 +13,19 @@ var metaCmd = &cobra.Command{
 	},
 }
 
+var profileCmd = &cobra.Command{
+	Use:   "profile",
+	Short: "Get profile information",
+	Args:  cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		err := api.MetaGetProfile()
+		errorhelper.ExitError(err)
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(metaCmd)
+
+	metaCmd.AddCommand(profileCmd)
+	profileCmd.PersistentFlags().BoolVarP(&api.MetaEdit, "edit", "e", false, "Edit profile information")
 }
