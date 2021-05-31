010011cec202e6b5f855a4ed7c5aac806f409025
Thorben Günther <admin@xenrox.net>
2021-05-31 15:42:31 +0200

    todo: Make ticket api behave like patch api

--- cmd/todo.go
+++ cmd/todo.go
@@ -13,8 +13,15 @@ var todoCmd = &cobra.Command{
 	},
 }
 
-var ticketsCmd = &cobra.Command{
-	Use:   "tickets",
+var ticketCmd = &cobra.Command{
+	Use:   "ticket",
+	Short: "Work on tickets",
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+var ticketListCmd = &cobra.Command{
+	Use:   "list",
 	Short: "List tickets",
 	Run: func(cmd *cobra.Command, args []string) {
 		err := api.PrintTickets(args)
@@ -25,13 +32,15 @@ var ticketsCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(todoCmd)
 
-	todoCmd.AddCommand(ticketsCmd)
-	ticketsCmd.Flags().StringVarP(&api.TicketStatus, "status", "s", "reported", "Ticket status")
-	ticketsCmd.RegisterFlagCompletionFunc("status", func(cmd *cobra.Command, args []string, toComplete string) (
+	todoCmd.AddCommand(ticketCmd)
+
+	ticketCmd.AddCommand(ticketListCmd)
+	ticketListCmd.Flags().StringVarP(&api.TicketStatus, "status", "s", "reported", "Ticket status")
+	ticketListCmd.RegisterFlagCompletionFunc("status", func(cmd *cobra.Command, args []string, toComplete string) (
 		[]string, cobra.ShellCompDirective) {
 		return []string{"reported", "confirmed", "in_progress", "pending", "resolved", "all"},
 			cobra.ShellCompDirectiveNoFileComp
 	})
-	ticketsCmd.Flags().StringVarP(&api.TrackerName, "tracker", "t", "", "Tracker name")
-	ticketsCmd.RegisterFlagCompletionFunc("tracker", completeNoFiles)
+	ticketListCmd.Flags().StringVarP(&api.TrackerName, "tracker", "t", "", "Tracker name")
+	ticketListCmd.RegisterFlagCompletionFunc("tracker", completeNoFiles)
 }
